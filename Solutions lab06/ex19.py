13c6ee8547cd026df39f89186aa4ba9f175d56f7
Vasco Pires <vasco.l.pires@tecnico.ulisboa.pt>
2022-12-08 01:19:44 +0000

    changes in 19

--- Solutions lab06/ex19.py
+++ Solutions lab06/ex19.py
@@ -1,43 +1,31 @@
-def computeGCD(x,y):
-    gcd = 0
-    if x > y:
-        small = y
-    else:
-        small = x
-    for i in range(1, small + 1):
-        if (x % i == 0) and (y % i == 0):
-            gcd = i
+def gcd(a: int, b: int):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
-    return gcd
-def cria_racional(first,second):
-    if isinstance(first,int) and isinstance(second,int) and second!=0:
-        sure = computeGCD(abs(first),abs(second))
-        one = abs(second)//sure
-        two = abs(first)//sure if (second > 0 and first > 0) or (second < 0 and first < 0) else -(abs(first)//sure)
-        return {"d" : one, "n" : two}
-    elif second == 0:
+
+def cria_racional(num: int, den: int):
+    if den == 0:
         raise ValueError("o denominador não pode ser 0")
-    else:
-        raise ValueError("os números devem ser inteiros")
-def escreve_racional(dic):
-    dic = cria_racional(dic["n"],dic["d"])
-    n = dic["n"]
-    d = dic["d"]
-    print(f"{n}/{d}")
-    return n,d
-def soma_racionais(d1,d2):
-   n1,d1 = map(int,escreve_racional(d1))
-   n2,d2 = map(int,escreve_racional(d2))
-   ntotal = 0
-   dtotal = 0
-   if d1==d2:
-       dtotal = d1
-       ntotal = n1+n2
-   else:
-       n1 = d2*n1
-       n2 = d1*n2
-       dtotal = d1*d2
-       ntotal = n1+n2
-   return {"d" : dtotal, "n" : ntotal}
 
-print(soma_racionais({'d': 6, 'n': 4},{'d': 3, 'n': 2}))
+    if not isinstance(num, int) or not isinstance(den, int):
+        raise ValueError("os números devem ser inteiros")
+
+    div_by = gcd(num, den)
+    return {
+        "n": num // div_by,
+        "d": den // div_by
+    }
+
+
+def escreve_racional(num: dict):
+    num = cria_racional(num["n"], num["d"])
+    print(f"{num['n']}/{num['d']}")
+    return
+
+
+def soma_racionais(n1: dict, n2: dict):
+    return cria_racional(
+        n1["n"] * n2["d"] + n2["n"] * n1["d"],
+        n1["d"] * n2["d"]
+    )
